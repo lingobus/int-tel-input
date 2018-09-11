3be9ade63a2a50559b833afe8caaf5d6be083215
Johnny Mikhael <johnny.mickael@gmail.com>
2018-09-11 03:06:12 +0200

    auto export into view component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import IntTelInput from './main.vue'
+import IntTelInput from './main.vue';
 
-export default IntTelInput
+export default IntTelInput;
+
+if (window && window.Vue) {
+    window.Vue.component('int-tel-input', IntTelInput);
+}
